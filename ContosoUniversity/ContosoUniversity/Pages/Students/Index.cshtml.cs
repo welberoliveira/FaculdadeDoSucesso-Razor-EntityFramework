f55cd248f8ed85826439f79c5a532e2e4eb2b7ec
welberoliveira <welberoliveira3@gmail.com>
2022-08-30 13:14:17 -0300

    Inclusão de ordenação dos campos da pagina Student

--- ContosoUniversity/ContosoUniversity/Pages/Students/Index.cshtml.cs
+++ ContosoUniversity/ContosoUniversity/Pages/Students/Index.cshtml.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ContosoUniversity.Data;
 using ContosoUniversity.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace ContosoUniversity.Pages.Students
 {
@@ -19,25 +15,40 @@ namespace ContosoUniversity.Pages.Students
         }
 
         public string NameSort { get; set; }
+        public string FirstSort { get; set; }
         public string DateSort { get; set; }
         public string CurrentFilter { get; set; }
         public string CurrentSort { get; set; }
 
         public IList<Student> Students { get; set; }
 
-        public async Task OnGetAsync(string sortOrder)
+        public async Task OnGetAsync(string sortOrder, string searchString)
         {
             // using System;
             NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            FirstSort = sortOrder == "First" ? "first_desc" : "First";
+
+            CurrentFilter = searchString;
 
             IQueryable<Student> studentsIQ = from s in _context.Students
                                              select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                studentsIQ = studentsIQ.Where(s => s.LastName.Contains(searchString)
+                                       || s.FirstMidName.Contains(searchString));
+            }
 
             switch (sortOrder)
             {
                 case "name_desc":
                     studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
+                    break;
+                case "First":
+                    studentsIQ = studentsIQ.OrderBy(s => s.FirstMidName);
+                    break;
+                case "first_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.FirstMidName);
                     break;
                 case "Date":
                     studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate);
